5622de8a8bc39027f612580bfe94024fcd0207f1
i.ivanov.it@gmail.com <>
2012-10-16 10:06:02 -0500

    [Update] Encapsulated functionality inside a class [Add] Styles

--- sample-connection/scripts/main.js
+++ sample-connection/scripts/main.js
@@ -1,39 +1,52 @@
 document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("touchstart", function() {}, false);
+
+var connectionInfo;
 
 function onDeviceReady() {
-    checkConnection();
-    
-    var buttonCheckConnection = document.getElementById("buttonCheckConnection");
-    buttonCheckConnection.addEventListener("click", checkConnection, false);
-}
-  
-function checkConnection() {
-    // Note that iOS can only report whether the device is on a cellular connection,
-    // not of what type, thus it will always report as CELL_2G
-    var networkState = navigator.network.connection.type,
-        messageConnectionType = document.getElementById("messageConnectionType"),
-        currentTimeDiv = document.getElementById("currentTime");
-    
-    //Display the connection type
-    messageConnectionType.innerText = networkState;
-    
-    // Display the time of the test
-    currentTimeDiv.innerText = formatTime();
+	connectionInfo = new ConnectionInfo();
+	connectionInfo.checkConnection();
 }
 
-function formatTime() {
-    var currentDate = new Date();
-    var currentHour = currentDate.getHours();
-    var currentMinute = currentDate.getMinutes();
-    var currentSecond = currentDate.getSeconds();
-    return currentHour + ":" + fixTime(currentMinute) + ":" + fixTime(currentSecond);
-     
-    function fixTime(value) {
-        if (value < 10) {
-            return "0" + value;
-        }
-        else {
-            return value;
-        }
-    }
+function ConnectionInfo() {
+	this.init();
+}
+
+ConnectionInfo.prototype = {
+	init: function() {
+		var buttonCheckConnection = document.getElementById("buttonCheckConnection"),
+		that = this;
+		buttonCheckConnection.addEventListener("click",
+											   function() {
+												   that.checkConnection.apply(that, arguments)
+											   } ,
+											   false);
+	},
+	checkConnection: function() {
+		var networkState = navigator.network.connection.type,
+		messageConnectionType = document.getElementById("messageConnectionType"),
+		currentTimeDiv = document.getElementById("currentTime"),
+		that = this;
+        
+		messageConnectionType.innerText = networkState;
+        
+		currentTimeDiv.innerText = that.formatTime();
+	},
+    
+	formatTime: function() {
+		var currentDate = new Date();
+		var currentHour = currentDate.getHours();
+		var currentMinute = currentDate.getMinutes();
+		var currentSecond = currentDate.getSeconds();
+		return currentHour + ":" + fixTime(currentMinute) + ":" + fixTime(currentSecond);
+         
+		function fixTime(value) {
+			if (value < 10) {
+				return "0" + value;
+			}
+			else {
+				return value;
+			}
+		}
+	}
 }
