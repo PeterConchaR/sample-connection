00a54f21f9ca238be88285dfc334f3ecb374101f
Steve Tsokev <stzokev@gmail.com>
2013-06-18 09:46:32 -0500

    refactored code

--- sample-connection/scripts/main.js
+++ sample-connection/scripts/main.js
@@ -1,21 +1,21 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 //Activate :active state on devices
-document.addEventListener("touchstart", function() {}, false);
+document.addEventListener("touchstart", function() {
+}, false);
 
 function onDeviceReady() {
-    navigator.splashscreen.hide();
+	navigator.splashscreen.hide();
 	var connectionInfo = new ConnectionApp();
 	connectionInfo.run();
 }
 
 function ConnectionApp() {
-	
 }
  
 ConnectionApp.prototype = {
 	run: function() {
 		var that = this,
-		    buttonCheckConnection = document.getElementById("buttonCheckConnection");
+		buttonCheckConnection = document.getElementById("buttonCheckConnection");
 		
 		buttonCheckConnection.addEventListener("click",
 											   function() {
@@ -27,30 +27,14 @@ ConnectionApp.prototype = {
 	
 	_checkConnection: function() {
 		var that = this,
-    		networkState = navigator.network.connection.type,
-    		messageConnectionType = document.getElementById("messageConnectionType"),
-    		currentTimeDiv = document.getElementById("currentTime");
+		networkState = navigator.network.connection.type,
+		messageConnectionType = document.getElementById("messageConnectionType"),
+		currentTimeDiv = document.getElementById("currentTime");
         
 		messageConnectionType.innerText = networkState;
-        
-		currentTimeDiv.innerText = that._formatTime();
+		var now = new Date().toTimeString();
+		
+		currentTimeDiv.innerText = now;
 	},
     
-	_formatTime: function() {
-		var currentDate = new Date(),
-		    currentHour = currentDate.getHours(),
-		    currentMinute = currentDate.getMinutes(),
-		    currentSecond = currentDate.getSeconds();
-		
-        return currentHour + ":" + fixTime(currentMinute) + ":" + fixTime(currentSecond);
-
-		function fixTime(value) {
-			if (value < 10) {
-				return "0" + value;
-			}
-			else {
-				return value;
-			}
-		}
-	}
 }
